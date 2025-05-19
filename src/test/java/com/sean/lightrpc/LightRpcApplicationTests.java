91a0790d728e1380429769d5a11e380dce226ddd
seanking <152>
2025-05-19 22:23:39 +0800

    Init the project

---
+++ src/test/java/com/sean/lightrpc/LightRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sean.lightrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
